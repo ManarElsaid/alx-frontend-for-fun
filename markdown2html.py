33ac460f7bb2fbac6dae7c0dcd58484c5ebda420
ManarElsaid <elsaidmanar@gmail.com>
2024-04-28 22:57:54 +0300

    4

--- markdown2html.py
+++ markdown2html.py
@@ -18,7 +18,8 @@ if __name__ == "__main__":
 
     with open(sys.argv[1]) as read:
         with open(sys.argv[2], 'w') as html:
-            unordered_list_start, ordered_list_start = False, False
+            unordered_list_start, ordered_list_start, paragraph =
+            False, False, False
             for line in read:
                 line = line.replace('**', '<b>', 1)
                 line = line.replace('**', '</b>', 1)
@@ -55,6 +56,17 @@ if __name__ == "__main__":
                     html.write('</ol>\n')
                     ordered_list_start = False
 
+                if not (heading_count or unordered_list_start or
+                        ordered_list_start):
+                    if not paragraph and length > 1:
+                        html.write('<p>\n')
+                        paragraph = True
+                    elif length > 1:
+                        html.write('<br/>\n')
+                    elif paragraph:
+                        html.write('</p>\n')
+                        paragraph = False
+
                 if length > 1:
                     html.write(line)
 
@@ -62,6 +74,6 @@ if __name__ == "__main__":
                 html.write('</ul>\n')
             if ordered_list_start:
                 html.write('</ol>\n')
-            # if paragraph:
-            #     html.write('</p>\n')
+            if paragraph:
+                html.write('</p>\n')
     exit(0)
