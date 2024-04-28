3520032be310ee807f0c69a4347f383226faebed
ManarElsaid <elsaidmanar@gmail.com>
2024-04-28 21:14:39 +0300

    1

--- markdown2html.py
+++ markdown2html.py
@@ -2,6 +2,8 @@
 """markdown to html """
 import sys
 import os
+import hashlib
+import re
 
 
 if __name__ == "__main__":
@@ -13,5 +15,19 @@ if __name__ == "__main__":
     if not os.path.exists(sys.argv[1]):
         print("Missing {}".format(sys.argv[1], file=sys.stderr))
         exit(1)
+
+    with open(sys.argv[1]) as read:
+        with open(sys.argv[2], 'w') as html:
+            for line in read:
+                length = len(line)
+                headings = line.strip("#")
+                heading_count = length - len(headings)
+                if 1 <= heading_count <= 6:
+                    line = '<h{}>'.format(
+                        heading_count) + headings.strip() + '</h{}>\n'.format(
+                        heading_count)
+                if length > 1:
+                    html.write(line)
+
 
     exit(0)
