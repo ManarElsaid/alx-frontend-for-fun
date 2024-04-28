9bf8b6c34e182e018134802319bf1d0d22af128b
ManarElsaid <elsaidmanar@gmail.com>
2024-04-28 19:54:39 +0300

    0

--- markdown2html.py
+++ markdown2html.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
               file=sys.stderr)
         exit(1)
 
-    if not os.path.isfile(sys.argv[1]):
+    if not os.path.exists(sys.argv[1]):
         print("Missing {}".format(sys.argv[1], file=sys.stderr))
         exit(1)
 
